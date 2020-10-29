07600db91827599a5e86e30b043ab791c7cf60b8
Prayangshu Biswas Hritwick <68601984+x44uarbdx@users.noreply.github.com>
2020-10-29 21:51:26 +0600

    1008 Salary

---
+++ ruby/1008.rb
@@ -0,0 +1,11 @@
+numfunc = Integer(gets.chomp)
+hrtrab = Integer(gets.chomp)
+valorhr  = gets.chomp.to_f
+salori = hrtrab*valorhr
+salario = '%.4f' % salori
+puts "NUMBER = #{numfunc}"
+puts "SALARY = U$ #{salario}"
+
+#Wrong Answer 5%
+# Author:- Prayangshu Biswas Hritwick <hritwickv@gmail.com>
+
