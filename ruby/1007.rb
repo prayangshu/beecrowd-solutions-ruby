ef53504111ee84399b951bbbdc39c7f5c74e7f3f
Prayangshu Biswas Hritwick <68601984+x44uarbdx@users.noreply.github.com>
2020-10-29 21:50:39 +0600

    1007 Difference

---
+++ ruby/1007.rb
@@ -0,0 +1,13 @@
+A = Integer(gets.chomp)
+B = Integer(gets.chomp)
+C = Integer(gets.chomp)
+D = Integer(gets.chomp)
+
+DIFERENCA = (A*B-C*D)
+
+puts "DIFERENCA = #{DIFERENCA}"
+
+
+
+# Author:- Prayangshu Biswas Hritwick <hritwickv@gmail.com>
+
