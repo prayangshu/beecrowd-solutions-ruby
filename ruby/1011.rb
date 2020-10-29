52582f70be48d2c9efb70ccb7c3875e3b4b013a6
Prayangshu Biswas Hritwick <68601984+x44uarbdx@users.noreply.github.com>
2020-10-29 21:53:57 +0600

    1011 Sphere

---
+++ ruby/1011.rb
@@ -0,0 +1,10 @@
+r = gets.chomp.to_f
+s=r**3
+s.to_i
+a= 1.333333333333333
+volumn= a*3.14159*s
+v= '%.3f' % volumn
+
+puts "VOLUME = #{v}"
+# Author:- Prayangshu Biswas Hritwick <hritwickv@gmail.com>
+
