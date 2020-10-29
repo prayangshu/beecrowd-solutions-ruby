35b1fb48ac891c9d7293c88bdc6dbcb806f1dcd4
Prayangshu Biswas Hritwick <68601984+x44uarbdx@users.noreply.github.com>
2020-10-29 21:48:49 +0600

    1006 Average 2

---
+++ ruby/1006.rb
@@ -0,0 +1,9 @@
+a = gets.to_f
+b = gets.to_f
+c = gets.to_f
+medi = (a/10*2)+(b/10*3)+(c/10*5)
+media= '%.1f' % medi
+puts "MEDIA = #{media}"
+
+# Author:- Prayangshu Biswas Hritwick <hritwickv@gmail.com>
+
