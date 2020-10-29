ceb4e99951c6784a2843239293ba4573720bcb62
Prayangshu Biswas Hritwick <68601984+x44uarbdx@users.noreply.github.com>
2020-10-29 09:23:13 -0700

    1015  Distance Between Two Points

--- ruby/1015.rb
+++ ruby/1015.rb
@@ -5,8 +5,8 @@ d = (x2 - x1) * (x2-x1)
 s = (y2 - y1) * (y2-y1)
 dist = d+s
 dists = Math.sqrt(dist)
-
-print("%0.4f" % dists)
+distso= '%.4f' % dists
+puts distso
 
 
 # Author:- Prayangshu Biswas Hritwick <hritwickv@gmail.com>
